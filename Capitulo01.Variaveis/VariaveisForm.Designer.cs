94621b99a40cc91ea5434e144d69ad3e767a0e9a
joao <joaohuila1@gmail.com>
2018-02-17 17:47:26 -0200

    Copitulo Variaveis

---
+++ Capitulo01.Variaveis/VariaveisForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Capitulo01.Variaveis
+{
+    partial class VariaveisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuSuperior = new System.Windows.Forms.MenuStrip();
+            this.operacoesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aritimeticaSubMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.reduzidasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operadoresIncrementaisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boolenasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logícaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ResultadoListBox = new System.Windows.Forms.ListBox();
+            this.ternariasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuSuperior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuSuperior
+            // 
+            this.MenuSuperior.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operacoesToolStripMenuItem});
+            this.MenuSuperior.Location = new System.Drawing.Point(20, 60);
+            this.MenuSuperior.Name = "MenuSuperior";
+            this.MenuSuperior.Size = new System.Drawing.Size(657, 24);
+            this.MenuSuperior.TabIndex = 0;
+            this.MenuSuperior.Text = "menuStrip1";
+            // 
+            // operacoesToolStripMenuItem
+            // 
+            this.operacoesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aritimeticaSubMenu,
+            this.reduzidasToolStripMenuItem,
+            this.operadoresIncrementaisToolStripMenuItem,
+            this.boolenasToolStripMenuItem,
+            this.logícaToolStripMenuItem,
+            this.ternariasToolStripMenuItem});
+            this.operacoesToolStripMenuItem.Name = "operacoesToolStripMenuItem";
+            this.operacoesToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.operacoesToolStripMenuItem.Text = "&Operações";
+            // 
+            // aritimeticaSubMenu
+            // 
+            this.aritimeticaSubMenu.Name = "aritimeticaSubMenu";
+            this.aritimeticaSubMenu.Size = new System.Drawing.Size(226, 22);
+            this.aritimeticaSubMenu.Text = "&Aritiméticas";
+            this.aritimeticaSubMenu.Click += new System.EventHandler(this.aritimeticaSubMenu_Click);
+            // 
+            // reduzidasToolStripMenuItem
+            // 
+            this.reduzidasToolStripMenuItem.Name = "reduzidasToolStripMenuItem";
+            this.reduzidasToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.reduzidasToolStripMenuItem.Text = "&Reduzidas";
+            this.reduzidasToolStripMenuItem.Click += new System.EventHandler(this.reduzidasToolStripMenuItem_Click);
+            // 
+            // operadoresIncrementaisToolStripMenuItem
+            // 
+            this.operadoresIncrementaisToolStripMenuItem.Name = "operadoresIncrementaisToolStripMenuItem";
+            this.operadoresIncrementaisToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.operadoresIncrementaisToolStripMenuItem.Text = "Incrementais e Decrementais";
+            this.operadoresIncrementaisToolStripMenuItem.Click += new System.EventHandler(this.operadoresIncrementaisToolStripMenuItem_Click);
+            // 
+            // boolenasToolStripMenuItem
+            // 
+            this.boolenasToolStripMenuItem.Name = "boolenasToolStripMenuItem";
+            this.boolenasToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.boolenasToolStripMenuItem.Text = "&Boolenas";
+            this.boolenasToolStripMenuItem.Click += new System.EventHandler(this.boolenasToolStripMenuItem_Click);
+            // 
+            // logícaToolStripMenuItem
+            // 
+            this.logícaToolStripMenuItem.Name = "logícaToolStripMenuItem";
+            this.logícaToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.logícaToolStripMenuItem.Text = "Logíca";
+            this.logícaToolStripMenuItem.Click += new System.EventHandler(this.logícaToolStripMenuItem_Click);
+            // 
+            // ResultadoListBox
+            // 
+            this.ResultadoListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ResultadoListBox.FormattingEnabled = true;
+            this.ResultadoListBox.Location = new System.Drawing.Point(20, 84);
+            this.ResultadoListBox.Name = "ResultadoListBox";
+            this.ResultadoListBox.Size = new System.Drawing.Size(657, 246);
+            this.ResultadoListBox.TabIndex = 1;
+            this.ResultadoListBox.SelectedIndexChanged += new System.EventHandler(this.ResultadoListBox_SelectedIndexChanged);
+            // 
+            // ternariasToolStripMenuItem
+            // 
+            this.ternariasToolStripMenuItem.Name = "ternariasToolStripMenuItem";
+            this.ternariasToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.ternariasToolStripMenuItem.Text = "Ternarias";
+            this.ternariasToolStripMenuItem.Click += new System.EventHandler(this.ternariasToolStripMenuItem_Click);
+            // 
+            // VariaveisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 350);
+            this.Controls.Add(this.ResultadoListBox);
+            this.Controls.Add(this.MenuSuperior);
+            this.MainMenuStrip = this.MenuSuperior;
+            this.Name = "VariaveisForm";
+            this.Text = "Variaveis e Operações";
+            this.MenuSuperior.ResumeLayout(false);
+            this.MenuSuperior.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuSuperior;
+        private System.Windows.Forms.ListBox ResultadoListBox;
+        private System.Windows.Forms.ToolStripMenuItem operacoesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aritimeticaSubMenu;
+        private System.Windows.Forms.ToolStripMenuItem reduzidasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operadoresIncrementaisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boolenasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logícaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ternariasToolStripMenuItem;
+    }
+}
+
---
+++ Capitulo01.Variaveis/VariaveisForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capitulo01.Variaveis
+{
+    public partial class VariaveisForm : MetroFramework.Forms.MetroForm
+    {
+        int _x = 32;
+        int _w = 45;
+        int _y = 16;
+        int _z = 32;
+
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ResultadoListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void aritimeticaSubMenu_Click(object sender, EventArgs e)
+        {
+
+            //string nomeCliente = "Manuel";
+            //bool habilitado = false;
+
+            //DateTime date = new DateTime();
+            //var @class = " 2 Ano";
+
+            //dados considerados tipos primitivos
+            int a = 23;
+            int c = 30;
+            int d = 34;
+
+            ResultadoListBox.Items.Add("O Resultado do A é " + a).ToString();
+            NovaLinha();
+
+            NovaLinha();
+            //concatenando as Variaveis
+            //Duplicando o (-) para 500
+            NovaLinha();
+
+            ResultadoListBox.Items.Add($"O Resultado do D é {d} ");
+            NovaLinha();
+
+            //somando as Variaveis 
+
+            ResultadoListBox.Items.Add("C * D =" + (c * d));
+            ResultadoListBox.Items.Add("C % A =" + (c % a));
+            ResultadoListBox.Items.Add("D + A =" + (a + d));
+            ResultadoListBox.Items.Add("C - D =" + (c - d));
+        }
+
+        private void reduzidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var x = 5;
+            ResultadoListBox.Items.Add("X =" + x);
+            //x = x - 3; ou 
+            x -= 3;
+            ResultadoListBox.Items.Add("X  =" + x);
+        }
+
+        private void operadoresIncrementaisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int incremental;
+            incremental = 5;
+            ResultadoListBox.Items.Add("Pre-incremental");
+            NovaLinha();
+
+            ResultadoListBox.Items.Add("incremental = " + incremental);
+            ResultadoListBox.Items.Add("2 + ++ incremental = " + (2 + ++incremental));
+            ResultadoListBox.Items.Add("2 Pos Incremental = " + (+incremental));
+            NovaLinha();
+
+            incremental = 5;
+            ResultadoListBox.Items.Add("Pos-incremental");
+            NovaLinha();
+            ResultadoListBox.Items.Add("incremental = " + incremental);
+            ResultadoListBox.Items.Add("2 + ++ incremental = " + (2 + incremental++));
+            ResultadoListBox.Items.Add("2 Pos Incremental = " + (+incremental));
+            NovaLinha();
+        }
+
+        public void NovaLinha()
+        {
+            ResultadoListBox.Items.Add(new string('-', 500));
+
+        }
+
+        private void boolenasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MostrarVariaveis();
+
+        }
+
+        //para criar um metado automatico, basta seleceionar o que precisas inserir ou criar o metodo, com os atalhos do teclado ctr+r+m ele
+        //    criar o metdo para voce so basta renomear o metodo
+
+        private void MostrarVariaveis()
+        {
+            ResultadoListBox.Items.Add("valor de X = " + _x);
+            ResultadoListBox.Items.Add("valor de w = " + _w);
+            ResultadoListBox.Items.Add("valor de Y = " + _y);
+            ResultadoListBox.Items.Add("valor de Z " + _z);
+            NovaLinha();
+
+            ResultadoListBox.Items.Add("W <= X + " + (_w <= _x));
+            ResultadoListBox.Items.Add("X == Z + + " + (_z == _x));
+            ResultadoListBox.Items.Add("X != Z + + " + (_z != _x));
+            NovaLinha();
+        }
+         
+        private void logícaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResultadoListBox.Items.Add(" Verdadeiro ou Falso");
+            NovaLinha();
+
+            MostrarVariaveis();
+            ResultadoListBox.Items.Add("_W <= _X || (ou) _Y == 16 " +(_w <=_x || _y == 16) );
+            ResultadoListBox.Items.Add("_X == _Z  &&  _Z != _X  " + (_x <= _z && _z != _x));
+            ResultadoListBox.Items.Add("!(_Y > _W) = " + (!(_y > _w)));
+
+        }
+
+        
+
+        private void ternariasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int ano;
+            ano = 2014;
+            ResultadoListBox.Items.Add("Ano" + ano);
+            //concatenar string if ternario
+            ResultadoListBox.Items.Add(string.Format ("O Ano {0} é Bisexto? {1}.",ano,
+               DateTime.IsLeapYear(2014)? "SIM":"NÂO"));
+
+            //if (ano% 4 == 0)
+            //
+            //    MessageBox.Show("O Ano é Bisexto");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("O Ano não é Bisexto");
+            //}
+
+            NovaLinha();
+             ano = 2016;
+            ResultadoListBox.Items.Add("Ano" + ano);
+            //concatenar string if ternario
+            ResultadoListBox.Items.Add(string.Format("O Ano {0} é Bisexto? {1}.", ano,
+                               DateTime.IsLeapYear(2016) ? "SIM" : "NÂO"));
+
+            //ano % 4 == 0 ? "SIM" : "NÂO"));
+        }
+    }
+}
